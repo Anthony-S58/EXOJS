0c035194dbcc3b74e53bd3ec9840280bca957699
Anthony-S58 <tonysim01@hotmail.com>
2021-07-13 16:44:01 +0200

    exo17 terminé

--- lvl2/js/exo17.js
+++ lvl2/js/exo17.js
@@ -1,1 +1,16 @@
 //Code à faire ici
+
+
+let para = document.getElementsByClassName('para');
+console.log(para);
+
+
+let tag = document.getElementsByTagName('span');
+console.log(tag);
+
+
+let truc = document.getElementsByClassName('truc')[0];
+console.log(truc);
+
+let all = document.getElementsByClassName('all');
+console.log(all);
