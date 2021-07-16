d553a2798aff3a312ffb0428f4d84eab3886489c
Anthony-S58 <tonysim01@hotmail.com>
2021-07-16 16:00:57 +0200

    défi20 terminé

--- lvl2/js/exo20.js
+++ lvl2/js/exo20.js
@@ -1,1 +1,6 @@
 //Code à faire ici
+
+let hello = document.getElementById("text").textContent="Hello World !";
+
+let div = document.querySelector("#html");
+div.innerHTML = '<p>Je suis maintenant un paragraphe</p>';
