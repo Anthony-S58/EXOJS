79112436507de9ef50b4b18e84cf8e2cd4e66598
Anthony <tonysim01@hotmail.com>
2021-07-21 20:19:11 +0200

    exo22 terminé

--- lvl3/js/exo22.js
+++ lvl3/js/exo22.js
@@ -1,1 +1,12 @@
 //Code à faire ici
+
+let para = document.getElementById("clickOnMe");
+
+para.addEventListener('click', function() {
+
+    console.log("cliquer sur le paragraphe d'id='clickOnMe'")
+
+});
+
+// manque bonus
+
