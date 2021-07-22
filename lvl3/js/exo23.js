ee439d91953e7c4182fac56483b3292c244ffef4
Anthony <tonysim01@hotmail.com>
2021-07-22 17:51:42 +0200

    exo23 terminé

--- lvl3/js/exo23.js
+++ lvl3/js/exo23.js
@@ -1,1 +1,20 @@
 //Code à faire ici
+
+let gras = document.getElementById("toggle");
+
+gras.addEventListener('click', function() {
+    // addEventlistener au click sur la div toggle
+
+    if (gras.classList==0) {
+        // si la div vaut 0 alors on ajoute la class toggle
+        gras.classList.add('toggle');
+    console.log('test');  
+    }
+
+    else {
+        // sinon elle vaut 1 alors on retire la class toggle
+        gras.classList.remove('toggle');
+        console.log('test2');
+ 
+
+}});
